857db5b36a3615fab32bcf4432c3cb0d507e701d
Anthony Racioppo <ajrac@udel.edu>
2023-03-20 16:04:50 -0400

    Set up initial scenes, logic for cats ordering, drink creation, send and clear buttons, preloading

---
+++ src/PreloadScene.ts
@@ -0,0 +1,28 @@
+import Phaser from 'phaser'
+
+export default class HelloWorldScene extends Phaser.Scene {
+	
+
+	constructor() {
+		super('PreloadScene')
+	}
+
+	preload() {
+		this.load.image('titleScreen', 'assets/titleScreen.png');
+		this.load.image('background', 'assets/background.png');
+		this.load.image('orangeCat', 'assets/orangeCat.jpg');
+		this.load.image('whiteCat', 'assets/whiteCat.jpg');
+		this.load.image('cup', 'assets/cup.png');
+		this.load.image('oranges', 'assets/oranges.jpg');
+		this.load.image('vanilla', 'assets/vanilla.jpg');
+		this.load.image('instruction1', 'assets/instruction1.png');
+		this.load.image('clear', 'assets/clear.png');
+		this.load.image('send', 'assets/send.png');
+		this.load.image('winScene', 'assets/winScreen.png');
+	}
+
+	create() {
+		this.scene.start('TitleScene');
+	}
+
+}
---
+++ src/TitleScene.ts
@@ -0,0 +1,22 @@
+import Phaser from 'phaser'
+
+export default class titleScene extends Phaser.Scene{
+
+    private button: any;
+
+    constructor(){
+        super({key: 'TitleScene'});
+    }
+// TitleScene code lifted from Katarina Pfeifer and Anna McCarter
+    create(){
+        //If they click anywhere it moves on to the tutorial
+        this.button=this.add.image(this.scale.width/2, this.scale.height/2, "titleScreen")
+        .setInteractive()
+        .on('pointerdown', ()=>this.goToTutorial());
+        this.button.setScale(0.8);
+    }
+
+    goToTutorial(){
+        this.scene.start('tutorialScene');
+    }
+}
