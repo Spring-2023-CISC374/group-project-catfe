ec7503992bc9f49438c014b67ac5b306094dac56
Anthony Racioppo <ajrac@udel.edu>
2023-04-25 14:27:06 -0400

    Implemeted leveling system

--- src/winScene.ts
+++ src/winScene.ts
@@ -4,14 +4,24 @@ export default class winScene extends Phaser.Scene{
 
     private button: any;
     private moneyCount: any;
+    private levelCount: any;
+    private level: integer = 0;
 
     constructor(){
         super({key: 'winScene'});
     }
 
-    create(data: { money: integer; }){
+    create(data: { money: integer, level: integer; }){
+        this.level = data.level;
         this.add.image(this.scale.width/2, this.scale.height/2, "winScene")
         this.moneyCount = this.add.text(300, 16, 'Money: '+data.money, { fontSize: '32px', color: 'red'});
+        this.moneyCount = this.add.text(300, 48, 'Level: '+this.level, { fontSize: '32px', color: 'red'});
+        this.button=this.add.image(this.scale.width/2, this.scale.height/2, "oranges").setInteractive().on('pointerdown', ()=>this.backToGame());
+        this.button.setScale(0.8)
+    }
+    backToGame(){
+        this.level++;
+        this.scene.start('GameScene', {level: this.level});
     }
 
     
