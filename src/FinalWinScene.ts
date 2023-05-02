2ac632be953501a8a3877f68bbf2a78a93b63006
diyashah <diyashah@udel.edu>
2023-05-02 13:28:45 -0400

    changed end scene, level scenes, and game scene: added buttons and fixed aesthetics

--- src/FinalWinScene.ts
+++ src/FinalWinScene.ts
@@ -4,8 +4,12 @@ export default class FinalWinScene extends Phaser.Scene {
     }
   
     create(data: { money: integer }) {
+      this.add.image(this.scale.width/2, this.scale.height/2, "winScene")
       const { money } = data;
-      this.add.text(400, 300, `Your total tips: ${money}`, { fontSize: '32px', color: 'red' });
+      this.add.text(400, 300, `Your total tips: ${money}`, { font: '30px Avenir',
+      color: '#4a2511',
+      backgroundColor: 'white', 
+      padding: {x:20, y:20} });
   
       const replayButton = this.add.text(400, 400, 'Replay', { font: '32px Avenir', color: 'white', backgroundColor: '#4a2511', padding: {x:32, y:32}}).setInteractive();
       replayButton.on('pointerdown', () => {
