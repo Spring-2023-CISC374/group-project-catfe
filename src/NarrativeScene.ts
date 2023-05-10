ffc0322f110456abc51a3a0eb755f147c4beef15
Emma Frampton <eframpt@udel.edu>
2023-05-09 23:34:04 -0400

    Added music loop

--- src/NarrativeScene.ts
+++ src/NarrativeScene.ts
@@ -12,6 +12,7 @@ export default class narrativeScene extends Phaser.Scene{
 // TitleScene code lifted from Katarina Pfeifer and Anna McCarter
     create(){
         this.music = this.sound.add('backgroundMusic');
+        this.music.loop = true;
         this.music.play();
         //If they click anywhere it moves on to the next scene
         this.button=this.add.image(this.scale.width/2, this.scale.height/2, "narrativeScene")
